1147465b8ab48707c89ba54842638d6181ce1876
Max <fb9@water-dragon.ru>
2024-10-17 10:19:39 +0200

    v1.2
    - Theme changed to Gruvbox Material Dark
    - Added preview image to README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fb9.nvim
- My  neovim setup
+My neovim setup *for RUST*
+Currently with [Gruvbox Material Dark](https://github.com/sainnhe/gruvbox-material) theme
+# Preview
+![[Screenshot From 2024-10-17 10-01-11.png]]
--- nvim/lua/fb9/plugins/colorscheme.lua
+++ nvim/lua/fb9/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
-    -- "sainnhe/gruvbox-material",
-    "catppuccin/nvim",
+    "sainnhe/gruvbox-material",
+    -- "catppuccin/nvim",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-        -- vim.cmd("colorscheme gruvbox-material")
-        vim.cmd.colorscheme("catppuccin")
+        vim.cmd("colorscheme gruvbox-material")
+        -- vim.cmd.colorscheme("catppuccin")
     end
 }
 
